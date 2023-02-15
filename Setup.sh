22958249affa1e291e1a5c4b730a20df381b0b92
baselomar <125217474+baselomar@users.noreply.github.com>
2023-02-15 22:02:06 +0400

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,41 @@
+echo '
+
+
+████████╗             ██████╗              ██████╗             ██╗     
+╚══██╔══╝            ██╔═══██╗            ██╔═══██╗            ██║     
+   ██║               ██║   ██║            ██║   ██║            ██║     
+   ██║               ██║   ██║            ██║   ██║            ██║     
+   ██║               ╚██████╔╝            ╚██████╔╝            ███████╗
+   ╚═╝                ╚═════╝              ╚═════╝             ╚══════╝
+                                                                     Beta 1.0
+
+
+
+[1] - Created By - BASEL OMAR
+______________________________
+[2] - Information - This tool will help you download all kinds of tools
+_______________________________________________________________________
+'|lolcat -p 1.6 -a -d 2 -s 20.0
+echo -e "============================" | lolcat
+echo -e $green "Start Test${enda}";
+echo -e $red "Exit${enda}";
+echo -e "============================" | lolcat
+read -p "╰─Solect =" pil;
+
+
+
+case $pil in
+1) pkg install figlet
+echo -e "${y} Thes Tool Is For Testing"
+echo -e "${y} Hi Mi Name Is Basel"
+echo -e "${y} Its Work"
+
+echo
+
+;;
+
+2) cd && claer
+
+echo
+
+;;
